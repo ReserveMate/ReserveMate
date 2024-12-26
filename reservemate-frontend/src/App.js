0553cb83e3f088c6bec95dfdafb43aa2c5ecac2c
shop_owner_backend <you@example.com>
2024-12-26 13:34:37 +0530

    Added more frontend files and updates

---
+++ reservemate-frontend/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Register from './components/Register'; // Import Register component
+
+function App() {
+  return (
+    <div className="App">
+      <Register /> {/* Use Register component */}
+    </div>
+  );
+}
+
+export default App;
